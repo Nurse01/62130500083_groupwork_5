e88c7a7edbcd57a5e87f363f874e0b1fe2717bc7
Warathaya Rattachai <warathaya.nursewarathaya@mail.kmutt.ac.th>
2021-02-20 20:56:40 +0700

    [Finish]

--- 62130500083_groupwork_4/main.js
+++ 62130500083_groupwork_4/main.js
@@ -2,20 +2,41 @@ const app = {
     data() {
         return {
             items: [
-                { name: 'tumamink', image: 'images/tumamink.jpg', liked: false },
-                { name: 'yoda', image: 'images/yoda.jpg', liked: false },
-                { name: 'jubcream', image: 'images/jubcream.jpg', liked: false }
-            ]
+                { name: 'tumamink', image: 'images/tumamink.jpg', liked: false ,isShow: true},
+                { name: 'yoda', image: 'images/yoda.jpg', liked: false ,isShow: true},
+                { name: 'jubcream', image: 'images/jubcream.jpg', liked: false , isShow: true }
+            ],
+            search : true,
+            searching: "",
+            isShow : false,
+            showSrc: ""
         }
     },
     methods: {
         toggleDone(index) {
             this.items[index].liked = !this.items[index].liked
         },
-        clickSearch() {
-            console.log("clicked")
-            // this. = !this.items[index].liked
+        toggleSearch() {
+            this.search = !this.search
+            this.searching = "";
 
+        },
+        searchFunc(searching){
+             this.items.forEach( item=> {
+                if (item.name.includes(searching)) {
+                    item.isShow = true;
+                }else{
+                    item.isShow =false;
+                }
+            });        
+        },
+        show(item){
+            this.isShow = true;
+            this.showSrc = item.image;
+
+        },
+        closePreview(){
+            this.isShow = false;
         }
         
     },
@@ -24,6 +45,9 @@ const app = {
         countimage() {
             return this.items.length
         }
+    },
+    beforeUpdate() {
+        this.searchFunc(this.searching)
     }
 }
 Vue.createApp(app).mount('#app')
